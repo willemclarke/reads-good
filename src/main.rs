ec1374d8e2b8ad0805ac3ab40edbea1d3577a2b6
willemclarke <willem.2@hotmail.com>
2024-08-17 16:08:38 +1000

    feat: add inquire crate for asking user prompts

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 mod scraper;
+use inquire;
 
 // "https://www.goodreads.com/list/show/3810.Best_Cozy_Mystery_Series"
 fn main() -> Result<(), scraper::Error> {
-    let x = scraper::scrape(String::from(
-        "https://www.goodreads.com/list/show/3810.Best_Cozy_Mystery_Series",
-    ));
-    Ok(())
+    let listopia_url =
+        inquire::Text::new("Provide the listopia url you would like to scrape").prompt();
+
+    match listopia_url {
+        Ok(url) => {
+            scraper::scrape(String::from(url));
+            Ok(())
+        }
+        Err(err) => {
+            println!("Error reading provided url: {:?}", err);
+            Err(scraper::Error::UnableToRetrieveListopia)
+        }
+    }
 }
